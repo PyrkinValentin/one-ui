099b9b8b5c0e99955ad76d633b2bcdc2dcd46fd6
Валентин Пыркин <elemental-hero@yandex.by>
2025-01-08 16:47:15 +0300

    feat: ui link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,12 @@
 import { Container } from "@/shared/ui/layout"
-import { Divider } from "@/shared/ui/divider"
+import { Link } from "@/shared/ui/link"
 
 const Home = () => {
 	return (
 		<Container className="pt-12">
-			<div className="max-w-md">
-				<div className="space-y-1">
-					<h4 className="text-medium font-medium">NextUI Components</h4>
-					<p className="text-small text-default-400">Beautiful, fast and modern React UI library.</p>
-				</div>
-
-				<Divider className="my-4"/>
-
-				<div className="flex h-5 items-center space-x-4 text-small">
-					<div>Blog</div>
-					<Divider orientation="vertical"/>
-					<div>Docs</div>
-					<Divider orientation="vertical"/>
-					<div>Source</div>
-				</div>
-			</div>
+			<Link href="/" external showAnchorIcon={false} block>
+				Hello link
+			</Link>
 		</Container>
 	)
 }
---
+++ src/shared/ui/link/variants.ts
@@ -0,0 +1,90 @@
+import type { VariantProps } from "@/core/theme"
+
+import { tv } from "@/core/theme"
+
+export type LinkVariantsProps = VariantProps<typeof linkVariants>
+
+export const linkVariants = tv({
+	base: "relative inline-flex items-center outline-none",
+	variants: {
+		size: {
+			sm: "text-sm",
+			md: "text-md",
+			lg: "text-lg",
+		},
+		color: {
+			foreground: "text-foreground",
+			primary: "text-primary",
+			secondary: "text-secondary",
+			success: "text-success",
+			warning: "text-warning",
+			danger: "text-danger",
+		},
+		underline: {
+			none: "no-underline",
+			hover: "hover:underline",
+			always: "underline",
+			active: "active:underline",
+			focus: "focus:underline",
+		},
+		block: {
+			true: [
+				"px-2 py-1 hover:after:opacity-100",
+				"after:content-[''] after:inset-0 after:opacity-0 after:w-full after:h-full after:rounded-xl",
+				"after:transition-[background] after:absolute",
+			],
+			false: "hover:opacity-80 active:opacity-disabled transition-opacity",
+		},
+		external: {
+			true: "gap-1",
+		},
+		disabled: {
+			true: "opacity-disabled cursor-default pointer-events-none",
+			false: "focus-visible:z-10 focus-visible:outline-2 focus-visible:outline-focus focus-visible:outline-offset-2",
+		},
+		disableAnimation: {
+			true: "after:transition-none transition-none",
+		},
+	},
+	defaultVariants: {
+		size: "md",
+		color: "primary",
+		underline: "none",
+	},
+	compoundVariants: [
+		{
+			block: true,
+			color: "foreground",
+			className: "hover:after:bg-foreground/10",
+		},
+		{
+			block: true,
+			color: "primary",
+			className: "hover:after:bg-primary/20",
+		},
+		{
+			block: true,
+			color: "secondary",
+			className: "hover:after:bg-secondary/20",
+		},
+		{
+			block: true,
+			color: "success",
+			className: "hover:after:bg-success/20",
+		},
+		{
+			block: true,
+			color: "warning",
+			className: "hover:after:bg-warning/20",
+		},
+		{
+			block: true,
+			color: "danger",
+			className: "hover:after:bg-danger/20",
+		},
+		{
+			underline: ["hover", "always", "active", "focus"],
+			className: "underline-offset-4",
+		},
+	],
+})
