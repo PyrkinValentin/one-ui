16d2d300bb0c8414e01047787e30d0facfb77c97
Валентин Пыркин <elemental-hero@yandex.by>
2025-01-08 06:07:50 +0300

    feat: ui button, button group

--- src/shared/ui/button/index.tsx
+++ src/shared/ui/button/index.tsx
@@ -1,28 +1,120 @@
-import type { ButtonProps } from "./types"
+"use client"
 
-import { useMemo } from "react"
+import type { ButtonGroupContextValue, ButtonGroupProps, ButtonProps } from "./types"
+
+import { use, useMemo } from "react"
+
+import { createContext } from "react"
+import { isUndefined } from "@/shared/helpers/is-undefined"
 
 import { Spinner } from "@/shared/ui/spinner"
 
-import { buttonVariants } from "./variants"
+import { buttonGroupVariants, buttonVariants } from "./variants"
+
+const ButtonGroupContext = createContext<ButtonGroupContextValue>({})
+const useButtonGroupContext = () => use(ButtonGroupContext)
+
+export const ButtonGroup = (props: ButtonGroupProps) => {
+	const {
+		className,
+		variant,
+		color,
+		size,
+		rounded,
+		fullWidth,
+		disabled,
+		iconOnly,
+		disableAnimation,
+		children,
+		...restProps
+	} = props
+
+	const classNames = useMemo(() => {
+		return buttonGroupVariants({
+			className,
+			fullWidth,
+		})
+	}, [
+		className,
+		fullWidth,
+	])
+
+	const contextValue: ButtonGroupContextValue = {
+		variant,
+		color,
+		size,
+		rounded,
+		fullWidth,
+		disabled,
+		iconOnly,
+		disableAnimation,
+	}
+
+	return (
+		<ButtonGroupContext value={contextValue}>
+			<div
+				role="group"
+				className={classNames}
+				{...restProps}
+			>
+				{children}
+			</div>
+		</ButtonGroupContext>
+	)
+}
 
 export const Button = (props: ButtonProps) => {
+	const buttonGroupContext = useButtonGroupContext()
+
 	const {
 		startContent,
 		endContent,
 		spinnerProps,
 		className,
 		classNames,
+		variant,
+		size,
+		color,
+		rounded,
+		fullWidth,
 		loading,
+		disabled,
+		iconOnly,
+		disableAnimation,
 		children,
 		...restProps
-	} = props
+	} = {
+		...buttonGroupContext,
+		...props,
+	}
+
+	const inGroup = !isUndefined(buttonGroupContext)
 
 	const slots = useMemo(() => {
 		return buttonVariants({
+			variant,
+			size,
+			color,
+			rounded,
+			fullWidth,
 			loading,
+			disabled,
+			inGroup,
+			iconOnly,
+			disableAnimation,
 		})
-	}, [loading])
+	}, [
+		variant,
+		size,
+		color,
+		rounded,
+		fullWidth,
+		loading,
+		disabled,
+		inGroup,
+		iconOnly,
+		disableAnimation,
+	])
 
 	const wrapperContent = loading ? (
 		<div className={slots.wrapper({ className: classNames?.wrapper })}>
@@ -40,11 +132,20 @@ export const Button = (props: ButtonProps) => {
 
 	return (
 		<button
+			disabled={loading || disabled}
 			className={slots.base({ className: [className, classNames?.base] })}
 			{...restProps}
 		>
 			{wrapperContent}
-			{loading && <Spinner {...spinnerProps}/>}
+
+			{loading && (
+				<Spinner
+					color="current"
+					size="sm"
+					className={slots.spinner({ className: classNames?.spinner })}
+					{...spinnerProps}
+				/>
+			)}
 		</button>
 	)
 }
