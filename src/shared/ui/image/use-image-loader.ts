9ef8c66652688361c4d354eb4f4312e1c1250916
Валентин Пыркин <elemental-hero@yandex.by>
2025-02-11 22:03:09 +0300

    feat/image

---
+++ src/shared/ui/image/use-image-loader.ts
@@ -0,0 +1,49 @@
+import type { RefObject } from "react"
+import type { ComponentProps } from "@/shared/types/props"
+
+import { useEffect, useState } from "react"
+
+type UseImageLoaderOptions =
+	Pick<ComponentProps<"img">, "loading"> &
+	UseImageOwnOptions
+
+type UseImageOwnOptions = {
+	ref: RefObject<HTMLImageElement | null>
+}
+
+export const useImageLoader = (options: UseImageLoaderOptions) => {
+	const {
+		loading,
+		ref,
+	} = options
+
+	const [loaded, setLoaded] = useState(loading === "eager")
+
+	useEffect(() => {
+		const element = ref.current
+
+		if (!element || loading === "eager") return
+
+		if (element.complete && element.naturalWidth) {
+			queueMicrotask(() => {
+				setLoaded(true)
+			})
+		}
+
+		element.onload = () => {
+			setLoaded(true)
+		}
+
+		element.onerror = () => {
+			setLoaded(false)
+		}
+
+		return () => {
+			ref.current = null
+			element.onload = null
+			element.onerror = null
+		}
+	}, [loading, ref])
+
+	return loaded
+}
